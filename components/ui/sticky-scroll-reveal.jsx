5029aa0aa63307cd3e536f69b31109b201a8ff3c
ashiq-firoz <ashiqfiroz08@gmail.com>
2024-04-25 23:58:07 +0530

    update footer

--- components/ui/sticky-scroll-reveal.jsx
+++ components/ui/sticky-scroll-reveal.jsx
@@ -58,8 +58,9 @@ export const StickyScroll = ({
       animate={{
         backgroundImage: backgroundImgs[activeCard % backgroundImgs.length],
         backdropFilter: 'none',
+        
       }}
-      className="h-[30rem] overflow-y-auto flex justify-center relative space-x-10  p-10"
+      className="h-[80vh] overflow-y-auto flex justify-center relative space-x-10  p-10"
       ref={ref}
     >
       <div className="div relative flex items-start px-4">
